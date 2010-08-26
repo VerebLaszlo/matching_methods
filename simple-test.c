08150525ea7efab81f5a7ab6cd9855cd4aa3319a
uzsolt <udvzsolt@gmail.com>
2010-08-26 14:27:57 +0200

    * kiíratás * különböző hossz

--- simple-test.c
+++ simple-test.c
@@ -3,21 +3,23 @@
 #include "util_math.h"
 
 int main () {
-#define N 200
+    int N[] = {200,200};
     int i;
     Array signal[2];
 
-    signal[0].data = calloc(sizeof(double),N);
-    signal[0].length = N;
-
-    signal[1].data = calloc(sizeof(double),N);
-    signal[1].length = N;
-
-    for (i=0; i<N; i++) {
-        signal[0].data[i] = sin(i);
-        signal[1].data[i] = cos(i);
+    for (i=0; i<2; i++) {
+        signal[i].data = calloc(sizeof(double),N[i]);
+        signal[i].length = N[i];
     }
-    calculate_Phase_Shift(signal);
+
+    for (i=0; i<N[0]; i++) {
+        signal[0].data[i] = sin(10*FITTING_PI/N[0]*i);
+    }
+    for (i=0; i<N[1]; i++) {
+        signal[1].data[i] = cos(10*FITTING_PI/N[1]*i);
+    }
+
+    printf("Phase shift: %.4f\n",calculate_Phase_Shift(signal));
 
     return 0;
 }
